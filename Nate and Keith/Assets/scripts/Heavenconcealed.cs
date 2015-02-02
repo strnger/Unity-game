306954dd9b0af0a6ef86f15f1543f974f5f25867
strnger <zadco@Keiths-MacBook-Pro.local>
2015-02-02 17:39:16 -0600

    sign weaving
    Heaven concealed now requires the “i” “o” and then “p” keys to be
    pressed in sequence, but can be cancelled by pressing “z”. to fire the
    second meteor, press “p” after 4 seconds

--- Nate and Keith/Assets/scripts/Heavenconcealed.cs
+++ Nate and Keith/Assets/scripts/Heavenconcealed.cs
@@ -10,6 +10,8 @@ public class Heavenconcealed : MonoBehaviour {
 	public GameObject Meteor2_prefab;
 	Vector3 meteorpos = Vector3.zero;
 
+	bool FirstSignWeaved = false;
+	bool SecondsignWeaved = false;
 
 	// Use this for initialization
 	void Start () {
@@ -22,20 +24,31 @@ public class Heavenconcealed : MonoBehaviour {
 		Camera cam = Camera.main;
 		Quaternion temp = cam.transform.rotation;
 
-		if (Input.GetButtonDown ("Fire3") && !second_available) 
+		//the next 3 "if" statements see if the sign "iop" is weaved, and if so, throw the first meteor
+		if (Input.GetKeyDown ("i") && !second_available) 
+		{
+			FirstSignWeaved = true;
+		} 
+
+		if(FirstSignWeaved == true && Input.GetKeyDown ("o") && !second_available)
+		{
+			SecondsignWeaved = true;
+		}
+
+		if(SecondsignWeaved == true && Input.GetKeyDown ("p") && !second_available)
 		{
 			temp = cam.transform.rotation;
 			meteorpos = cam.transform.position;
 			meteorpos += cam.transform.forward * 200;
 			meteorpos.y += 200;
-
+			
 			GameObject meteor1 = (GameObject)Instantiate (Meteor1_prefab, meteorpos, temp);
 			meteor1.rigidbody.AddForce (0, 0, 0);
 			second_available = true;
-		} 
+		}
 
-
-		if(Input.GetButtonDown ("Fire3") && second_available && secondMeteorTime > 0 && secondMeteorTime < 6 && !second_fired)
+		//Second meteor
+		if(Input.GetButtonDown ("p") && second_available && secondMeteorTime > 0 && secondMeteorTime < 6 && !second_fired)
 		{
 			meteorpos.y += 1000;
 			GameObject meteor2 = (GameObject)Instantiate (Meteor2_prefab, meteorpos, temp);
@@ -43,16 +56,30 @@ public class Heavenconcealed : MonoBehaviour {
 			second_fired = true;
 		}
 
+		//This lets the user escape from the current sequence of signs
+		if (Input.GetButtonDown ("z") && !second_available /*this is needed, otherwise the player could hit Z after firing the first meteor, the fire another first one*/)
+		{
+			second_available = false;
+			second_fired = false;
+			secondMeteorTime = 10.0f;
+			FirstSignWeaved = false;
+			SecondsignWeaved = false;
+		}
+
+		//This prevents the player from firing the second immediately
 		if (second_available) {
 			
 			secondMeteorTime -= Time.deltaTime;
 		}
 
+		//This resets the meteor timer and signs, letting the player use them again
 		if (secondMeteorTime <= 0 && second_available) 
 		{
 			second_available = false;
 			second_fired = false;
 			secondMeteorTime = 10.0f;
+			FirstSignWeaved = false;
+			SecondsignWeaved = false;
 		}
 
 
